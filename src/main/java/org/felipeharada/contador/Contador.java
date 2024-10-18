1e1cfc7a7cae14fea910984ee8c71e85f3101cfb
Felipe Harada <harada.xd@gmail.com>
2024-10-18 01:37:03 -0300

    first commit

---
+++ src/main/java/org/felipeharada/contador/Contador.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.felipeharada.contador;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author felip
+ */
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+
+		try {
+			contar(parametroUm, parametroDois);
+
+		}catch (ParametrosInvalidosException exception) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException();
+		}
+		int contagem = parametroDois - parametroUm;
+		for (int i = 0; i <= contagem; i++) {
+			System.out.println(parametroUm + i);
+		}
+	}
+}
